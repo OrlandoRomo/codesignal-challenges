663585de5e5b9a5ef7db7a33d6e8c49b9478f8db
Orlando Romo <orlando.romo@improving.com>
2021-07-07 11:21:15 -0500

    clean main

--- main.go
+++ main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
 func main() {
-	fmt.Println(addBorder([]string{"abc", "ded"}))
-}
 
-func addBorder(picture []string) []string {
-	starLength := len(picture[0]) + 2
-	star := strings.Repeat("*", starLength)
-	for i := 0; i < len(picture); i++ {
-		picture[i] = string("*") + picture[i] + string("*")
-
-	}
-	picture = append([]string{star}, picture...)
-	picture = append(picture, star)
-	return picture
 }
